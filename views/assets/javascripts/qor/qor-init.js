b1703e9dde34a88386aeeba04bac111d6a7e3f92
jason weng <wengjianm@gmail.com>
2016-07-06 14:26:27 +0800

    fix(AJAX) clear dirty form checks when ajaxComplete

--- views/assets/javascripts/qor/qor-init.js
+++ views/assets/javascripts/qor/qor-init.js
@@ -3,3 +3,11 @@ $.fn.qorSliderAfterShow = {};
 
 // change Mustache tags from {{}} to [[]]
 window.Mustache.tags = ['[[', ']]'];
+
+$(document).ajaxComplete(function( event, xhr, settings ) {
+    if (settings.type == "POST" || settings.type == "PUT") {
+        $.fn.qorSlideoutBeforeHide = null;
+        window.onbeforeunload = null;
+    }
+
+});
