8a9a3d42ca4890c2f226e571cdc5ef5181150c36
Jinzhu <wosmvp@gmail.com>
2016-09-11 11:40:48 +0800

    Refactor select one

--- meta_select_one.go
+++ meta_select_one.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"path"
 	"reflect"
 
+	"github.com/jinzhu/gorm"
 	"github.com/qor/qor"
 	"github.com/qor/qor/resource"
 	"github.com/qor/qor/utils"
@@ -43,91 +45,54 @@ func (selectOneConfig SelectOneConfig) GetCollection(value interface{}, context 
 // ConfigureQorMeta configure select one meta
 func (selectOneConfig *SelectOneConfig) ConfigureQorMeta(metaor resource.Metaor) {
 	if meta, ok := metaor.(*Meta); ok {
-		meta.Type = "select_one"
-
-		// Set GetCollection
-		if selectOneConfig.Collection != nil {
-			selectOneConfig.SelectMode = "select"
-
-			if values, ok := selectOneConfig.Collection.([]string); ok {
-				selectOneConfig.getCollection = func(interface{}, *Context) (results [][]string) {
-					for _, value := range values {
-						results = append(results, []string{value, value})
-					}
-					return
-				}
-			} else if maps, ok := selectOneConfig.Collection.([][]string); ok {
-				selectOneConfig.getCollection = func(interface{}, *Context) [][]string {
-					return maps
-				}
-			} else if fc, ok := selectOneConfig.Collection.(func(interface{}, *qor.Context) [][]string); ok {
-				selectOneConfig.getCollection = func(record interface{}, context *Context) [][]string {
-					return fc(record, context.Context)
-				}
-			} else if fc, ok := selectOneConfig.Collection.(func(interface{}, *Context) [][]string); ok {
-				selectOneConfig.getCollection = fc
-			} else {
-				utils.ExitWithMsg("Unsupported Collection format for meta %v of resource %v", meta.Name, reflect.TypeOf(meta.baseResource.Value))
-			}
-		}
-
-		// Set GetCollection if normal select mode
-		if selectOneConfig.getCollection == nil {
-			if selectOneConfig.RemoteDataResource == nil {
-				fieldType := meta.FieldStruct.Struct.Type
-				for fieldType.Kind() == reflect.Ptr || fieldType.Kind() == reflect.Slice {
-					fieldType = fieldType.Elem()
-				}
-				selectOneConfig.RemoteDataResource = meta.baseResource.GetAdmin().NewResource(reflect.New(fieldType).Interface())
-			}
-
-			if selectOneConfig.SelectMode == "" {
-				selectOneConfig.SelectMode = "select_async"
-			}
-
-			selectOneConfig.getCollection = func(_ interface{}, context *Context) (results [][]string) {
-				cloneContext := context.clone()
-				cloneContext.setResource(selectOneConfig.RemoteDataResource)
-				searcher := &Searcher{Context: cloneContext}
-				searchResults, _ := searcher.FindMany()
-
-				reflectValues := reflect.Indirect(reflect.ValueOf(searchResults))
-				for i := 0; i < reflectValues.Len(); i++ {
-					value := reflectValues.Index(i).Interface()
-					scope := context.GetDB().NewScope(value)
-					results = append(results, []string{fmt.Sprint(scope.PrimaryKeyValue()), utils.Stringify(value)})
-				}
-				return
-			}
-		}
-
-		if selectOneConfig.SelectMode == "select_async" || selectOneConfig.SelectMode == "bottom_sheet" {
-			if remoteDataResource := selectOneConfig.RemoteDataResource; remoteDataResource != nil {
-				baseResource := meta.GetBaseResource().(*Resource)
-				Admin := baseResource.GetAdmin()
-				if remoteDataResource.params == "" {
-					remoteDataResource.params = fmt.Sprintf("!remote_data_searcher/%v/%v", baseResource.ToParam(), meta.GetName())
-					remoteDataSearcherController := &controller{Admin: Admin}
-					Admin.registerResourceToRouter(remoteDataSearcherController, remoteDataResource, "create", "update", "read", "delete")
-				}
-			} else {
-				utils.ExitWithMsg("RemoteDataResource not configured for meta %v", meta.Name)
-			}
-		}
-
 		// Set FormattedValuer
 		if meta.FormattedValuer == nil {
 			meta.SetFormattedValuer(func(record interface{}, context *qor.Context) interface{} {
 				return utils.Stringify(meta.GetValuer()(record, context))
 			})
 		}
+
+		selectOneConfig.prepareDataSource(meta.FieldStruct, meta.baseResource, "!remote_data_selector")
+
+		meta.Type = "select_one"
 	}
 }
 
 func (selectOneConfig *SelectOneConfig) ConfigureQORAdminFilter(filter *Filter) {
-	admin := filter.Resource.GetAdmin()
-	filter.Type = "select_one"
+	var structField *gorm.StructField
+	if field, ok := filter.Resource.GetAdmin().Config.DB.NewScope(filter.Resource.Value).FieldByName(filter.Name); ok {
+		structField = field.StructField
+	}
 
+	selectOneConfig.prepareDataSource(structField, filter.Resource, "!remote_data_filter")
+
+	filter.Type = "select_one"
+}
+
+func (selectOneConfig *SelectOneConfig) FilterValue(filter *Filter, context *Context) interface{} {
+	var (
+		prefix  = fmt.Sprintf("filters[%v].", filter.Name)
+		keyword string
+	)
+
+	if metaValues, err := resource.ConvertFormToMetaValues(context.Request, []resource.Metaor{}, prefix); err == nil {
+		if metaValue := metaValues.Get("Value"); metaValue != nil {
+			keyword = utils.ToString(metaValue.Value)
+		}
+	}
+
+	if keyword != "" && selectOneConfig.RemoteDataResource != nil {
+		result := selectOneConfig.RemoteDataResource.NewStruct()
+		clone := context.Clone()
+		clone.ResourceID = keyword
+		selectOneConfig.RemoteDataResource.CallFindOne(result, nil, clone)
+		return result
+	}
+
+	return keyword
+}
+
+func (selectOneConfig *SelectOneConfig) prepareDataSource(field *gorm.StructField, res *Resource, routePrefix string) {
 	// Set GetCollection
 	if selectOneConfig.Collection != nil {
 		selectOneConfig.SelectMode = "select"
@@ -150,20 +115,18 @@ func (selectOneConfig *SelectOneConfig) ConfigureQORAdminFilter(filter *Filter) 
 		} else if fc, ok := selectOneConfig.Collection.(func(interface{}, *Context) [][]string); ok {
 			selectOneConfig.getCollection = fc
 		} else {
-			utils.ExitWithMsg("Unsupported Collection format for filter %v of resource %v", filter.Name, reflect.TypeOf(filter.Resource.Value))
+			utils.ExitWithMsg("Unsupported Collection format")
 		}
 	}
 
 	// Set GetCollection if normal select mode
 	if selectOneConfig.getCollection == nil {
 		if selectOneConfig.RemoteDataResource == nil {
-			if field, ok := admin.Config.DB.NewScope(filter.Resource.Value).FieldByName(filter.Name); ok {
-				fieldType := field.StructField.Struct.Type
-				for fieldType.Kind() == reflect.Ptr || fieldType.Kind() == reflect.Slice {
-					fieldType = fieldType.Elem()
-				}
-				selectOneConfig.RemoteDataResource = filter.Resource.GetAdmin().NewResource(reflect.New(fieldType).Interface())
+			fieldType := field.Struct.Type
+			for fieldType.Kind() == reflect.Ptr || fieldType.Kind() == reflect.Slice {
+				fieldType = fieldType.Elem()
 			}
+			selectOneConfig.RemoteDataResource = res.GetAdmin().NewResource(reflect.New(fieldType).Interface())
 		}
 
 		if selectOneConfig.SelectMode == "" {
@@ -188,38 +151,13 @@ func (selectOneConfig *SelectOneConfig) ConfigureQORAdminFilter(filter *Filter) 
 
 	if selectOneConfig.SelectMode == "select_async" || selectOneConfig.SelectMode == "bottom_sheet" {
 		if remoteDataResource := selectOneConfig.RemoteDataResource; remoteDataResource != nil {
-			baseResource := filter.Resource
-			Admin := baseResource.GetAdmin()
 			if remoteDataResource.params == "" {
-				remoteDataResource.params = fmt.Sprintf("!remote_data_filter/%v/%v", baseResource.ToParam(), utils.ToParamString(filter.Name))
-				remoteDataSearcherController := &controller{Admin: Admin}
-				Admin.registerResourceToRouter(remoteDataSearcherController, remoteDataResource, "create", "update", "read", "delete")
+				remoteDataResource.params = path.Join(routePrefix, res.ToParam(), field.Name)
+				remoteDataSearcherController := &controller{Admin: res.GetAdmin()}
+				res.GetAdmin().registerResourceToRouter(remoteDataSearcherController, remoteDataResource, "create", "update", "read", "delete")
 			}
 		} else {
-			utils.ExitWithMsg("RemoteDataResource not configured for filter %v", filter.Name)
+			utils.ExitWithMsg("RemoteDataResource not configured")
 		}
 	}
-}
-
-func (selectOneConfig *SelectOneConfig) FilterValue(filter *Filter, context *Context) interface{} {
-	var (
-		prefix  = fmt.Sprintf("filters[%v].", filter.Name)
-		keyword string
-	)
-
-	if metaValues, err := resource.ConvertFormToMetaValues(context.Request, []resource.Metaor{}, prefix); err == nil {
-		if metaValue := metaValues.Get("Value"); metaValue != nil {
-			keyword = utils.ToString(metaValue.Value)
-		}
-	}
-
-	if keyword != "" && selectOneConfig.RemoteDataResource != nil {
-		result := selectOneConfig.RemoteDataResource.NewStruct()
-		clone := context.Clone()
-		clone.ResourceID = keyword
-		selectOneConfig.RemoteDataResource.CallFindOne(result, nil, clone)
-		return result
-	}
-
-	return keyword
 }
