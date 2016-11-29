95ab23c738dddae6dd6855f2a333f8b0e25c9863
jason weng <wengjianm@gmail.com>
2016-11-29 19:20:46 +0800

    feat(bottom-sheet) add normal submit indicator for buttom sheet form submit@3

--- views/assets/javascripts/qor/qor-bottomsheets.js
+++ views/assets/javascripts/qor/qor-bottomsheets.js
@@ -299,6 +299,7 @@
 
                         if (normalSubmit) {
                             $form.submit();
+                            return;
                         }
 
                         $('.qor-error').remove();
