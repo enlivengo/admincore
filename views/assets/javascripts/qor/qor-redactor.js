d6ea66347d1c55ea0a5e003c736fce29f6fe3041
jasonweng <wengjianm@gmail.com>
2017-03-23 15:34:04 +0800

    feat(redactor) upgrade redactor from 1.3.1 to 2.4 @2: add popover layout and js bug

--- views/assets/javascripts/qor/qor-redactor.js
+++ views/assets/javascripts/qor/qor-redactor.js
@@ -371,7 +371,7 @@
                             var $linkHtml = this.link.$linkHtml,
                                 description = this.link.description;
 
-                            if (name == 'link' && !this.link.insertedTriggered && $linkHtml.length && this.link.valueChanged && !this.link.clickCancel) {
+                            if (name == 'link' && !this.link.insertedTriggered && $linkHtml && $linkHtml.length && this.link.valueChanged && !this.link.clickCancel) {
                                 if (description) {
                                     $linkHtml.prop('title', description);
                                 } else {
