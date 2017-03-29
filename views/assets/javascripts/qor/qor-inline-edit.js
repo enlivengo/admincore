d32e4d2f89fa8e464d3eed96426105730ca0f3ed
jason weng <wengjianm@gmail.com>
2017-03-29 14:31:27 +0800

    deat(inlineedit) add js @2

--- views/assets/javascripts/qor/qor-inline-edit.js
+++ views/assets/javascripts/qor/qor-inline-edit.js
@@ -13,14 +13,18 @@
 
     'use strict';
 
-    const $window = $(window),
-        NAMESPACE = 'qor.inlineEdit',
+    const NAMESPACE = 'qor.inlineEdit',
         EVENT_ENABLE = 'enable.' + NAMESPACE,
         EVENT_DISABLE = 'disable.' + NAMESPACE,
         EVENT_CLICK = 'click.' + NAMESPACE,
+        EVENT_MOUSEENTER = 'mouseenter.' + NAMESPACE,
+        EVENT_MOUSELEAVE = 'mouseleave.' + NAMESPACE,
         CLASS_FIELD = '.qor-field',
         CLASS_FIELD_SHOW = '.qor-field__show',
-        CLASS_FIELD_EDIT = '.qor-field__edit';
+        CLASS_FIELD_EDIT = '.qor-field__edit',
+        CLASS_EDIT = '.qor-inlineedit__edit',
+        CLASS_SAVE = '.qor-inlineedit__save',
+        CLASS_CANCEL = '.qor-inlineedit__cancel';
 
     function QorInlineEdit(element, options) {
         this.$element = $(element);
@@ -36,27 +40,57 @@
         },
 
         bind: function() {
-
+            this.$element
+                .on(EVENT_MOUSEENTER, CLASS_FIELD_SHOW, this.showEditButton)
+                .on(EVENT_MOUSELEAVE, CLASS_FIELD_SHOW, this.hideEditButton)
+                .on(EVENT_CLICK, CLASS_CANCEL, this.hideEdit)
+                .on(EVENT_CLICK, CLASS_SAVE, this.saveEdit)
+                .on(EVENT_CLICK, CLASS_EDIT, this.showEdit);
         },
 
         unbind: function() {
 
         },
 
-        build: function() {
-
+        showEditButton: function() {
+            let $edit = $(QorInlineEdit.TEMPLATE_EDIT);
+            $edit.appendTo($(this));
         },
 
-        unbuild: function() {},
+        hideEditButton: function() {
+            $('.qor-inlineedit__edit').remove();
+        },
+
+        showEdit: function() {
+            let $parent = $(this).hide().closest('.qor-field').addClass('qor-inlineedit__field'),
+                $save = $(QorInlineEdit.TEMPLATE_SAVE);
+
+            $save.appendTo($parent.find(CLASS_FIELD_EDIT).show())
+            $parent.find(CLASS_FIELD_SHOW).hide();
+        },
+
+        hideEdit: function() {
+            let $parent = $(this).closest('.qor-field');
+            $(`${CLASS_EDIT},${CLASS_SAVE}`).remove();
+            $parent.find(CLASS_FIELD_EDIT).hide();
+            $parent.find(CLASS_FIELD_SHOW).show();
+        },
+
+        saveEdit: function() {
+            let $parent = $(this).closest('.qor-field');
+        },
 
         destroy: function() {
             this.unbind();
-            this.unbuild();
             this.$element.removeData(NAMESPACE);
         }
     };
 
     QorInlineEdit.DEFAULTS = {};
+
+    QorInlineEdit.TEMPLATE_EDIT = `<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored qor-inlineedit__edit" type="button"><i class="material-icons">mode_edit</i></button>`;
+    QorInlineEdit.TEMPLATE_SAVE = `<button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored qor-inlineedit__save" type="button"><i class="material-icons">done</i></button>
+                                    <button class="mdl-button mdl-js-button mdl-button--icon qor-inlineedit__cancel" type="button"><i class="material-icons">close</i></button>`;
 
     QorInlineEdit.plugin = function(options) {
         return this.each(function() {
