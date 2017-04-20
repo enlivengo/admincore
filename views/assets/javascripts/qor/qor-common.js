98bfb3da4346bb1f02e822beb80fcba7d310be5e
jason weng <wengjianm@gmail.com>
2017-04-20 16:40:42 +0800

    refactor window.confirm to custom alert @3

--- views/assets/javascripts/qor/qor-common.js
+++ views/assets/javascripts/qor/qor-common.js
@@ -1,9 +1,12 @@
 $(function() {
+
+    let $dialog = $('#dialog');
+
     $(document).on('keyup.qor.confirm', function(e) {
         if (e.which === 27) {
-            if ($('#dialog').length) {
+            if ($dialog.is(':visible')) {
                 setTimeout(function() {
-                    $('#dialog').remove();
+                    $dialog.hide();
                 }, 100);
             }
         }
@@ -12,31 +15,13 @@ $(function() {
             callback = window.QOR.qorConfirmCallback;
 
         $.isFunction(callback) && callback(value);
-        $('#dialog').remove();
+        $dialog.hide();
         window.QOR.qorConfirmCallback = undefined;
         return false;
     });
 
-    window.QOR.qorConfirmHtml = `<div id="dialog"><div class="mdl-dialog-bg"></div><div class="mdl-dialog">
-                      <div class="mdl-dialog__content">
-                        <p><i class="material-icons">warning</i></p>
-                        <p class="mdl-dialog__message">
-                          [[message]]
-                        </p>
-                      </div>
-                      <div class="mdl-dialog__actions">
-                        <button type="button" class="mdl-button mdl-button--raised mdl-button--colored dialog-button" data-type="confirm">Confirm</button>
-                        <button type="button" class="mdl-button dialog-button" data-type="">Cancel</button>
-                      </div>
-                    </div></div>`;
-
     window.QOR.qorConfirm = function(message, callback) {
-        let $dialog = $(window.Mustache.render(window.QOR.qorConfirmHtml, {
-            'message': message
-        }));
-
-        $('#dialog').remove();
-        $dialog.appendTo($('body'));
+        $dialog.show().find('.dialog-message').text(message);
         window.QOR.qorConfirmCallback = callback;
         return false;
     };
