37f637f72577eebb8f9961e1ad3b87582018a623
Jens Hult <jens@pinchos.se>
2017-05-08 14:37:36 +0200

    Json content type header set in json response.

--- transformer_json.go
+++ transformer_json.go
@@ -30,6 +30,7 @@ func (JSONTransformer) Encode(writer io.Writer, encoder Encoder) error {
 		result["error"] = err.Error()
 		js, _ = json.Marshal(result)
 	}
+	context.Writer.Header().Set("Content-Type", "application/json")
 
 	_, err = writer.Write(js)
 	return err
