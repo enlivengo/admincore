6050d2b0c67a325a08c2ebe5a911866738136245
Jinzhu <wosmvp@gmail.com>
2017-05-16 10:40:39 +0800

    Add default creating option for select one/many with bottom sheet

--- meta_select_many.go
+++ meta_select_many.go
@@ -13,6 +13,7 @@ import (
 // SelectManyConfig meta configuration used for select many
 type SelectManyConfig struct {
 	Collection               interface{} // []string, [][]string, func(interface{}, *qor.Context) [][]string, func(interface{}, *admin.Context) [][]string
+	DefaultCreating          bool
 	Placeholder              string
 	SelectionTemplate        string
 	SelectMode               string // select, select_async, bottom_sheet
@@ -35,12 +36,14 @@ func (selectManyConfig *SelectManyConfig) ConfigureQorMeta(metaor resource.Metao
 	if meta, ok := metaor.(*Meta); ok {
 		selectManyConfig.SelectOneConfig.Collection = selectManyConfig.Collection
 		selectManyConfig.SelectOneConfig.SelectMode = selectManyConfig.SelectMode
+		selectManyConfig.SelectOneConfig.DefaultCreating = selectManyConfig.DefaultCreating
 		selectManyConfig.SelectOneConfig.Placeholder = selectManyConfig.Placeholder
 		selectManyConfig.SelectOneConfig.RemoteDataResource = selectManyConfig.RemoteDataResource
 
 		selectManyConfig.SelectOneConfig.ConfigureQorMeta(meta)
 
 		selectManyConfig.RemoteDataResource = selectManyConfig.SelectOneConfig.RemoteDataResource
+		selectManyConfig.DefaultCreating = selectManyConfig.SelectOneConfig.DefaultCreating
 		meta.Type = "select_many"
 
 		// Set FormattedValuer
