d9c40a7373adf0b09840e047f810fba42e8026f6
jason weng <wengjianm@gmail.com>
2017-08-15 16:49:23 +0800

    fix(selectone) fix select one data input bug

--- views/assets/javascripts/qor/qor-select-one.js
+++ views/assets/javascripts/qor/qor-select-one.js
@@ -1,4 +1,4 @@
-(function (factory) {
+(function(factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as anonymous module.
     define(['jquery'], factory);
@@ -9,28 +9,26 @@
     // Browser globals.
     factory(jQuery);
   }
-})(function ($) {
-
+})(function($) {
   'use strict';
 
   let $body = $('body'),
-      $document = $(document),
-      Mustache = window.Mustache,
-      NAMESPACE = 'qor.selectone',
-      PARENT_NAMESPACE = 'qor.bottomsheets',
-      EVENT_CLICK = 'click.' + NAMESPACE,
-      EVENT_ENABLE = 'enable.' + NAMESPACE,
-      EVENT_DISABLE = 'disable.' + NAMESPACE,
-      EVENT_RELOAD = 'reload.' + PARENT_NAMESPACE,
-      CLASS_CLEAR_SELECT = '.qor-selected__remove',
-      CLASS_CHANGE_SELECT = '.qor-selected__change',
-      CLASS_SELECT_FIELD = '.qor-field__selected',
-      CLASS_SELECT_INPUT = '.qor-field__selectone-input',
-      CLASS_SELECT_TRIGGER = '.qor-field__selectone-trigger',
-      CLASS_PARENT = '.qor-field__selectone',
-      CLASS_SELECTED = 'is_selected',
-      CLASS_ONE = 'qor-bottomsheets__select-one';
-
+    $document = $(document),
+    Mustache = window.Mustache,
+    NAMESPACE = 'qor.selectone',
+    PARENT_NAMESPACE = 'qor.bottomsheets',
+    EVENT_CLICK = 'click.' + NAMESPACE,
+    EVENT_ENABLE = 'enable.' + NAMESPACE,
+    EVENT_DISABLE = 'disable.' + NAMESPACE,
+    EVENT_RELOAD = 'reload.' + PARENT_NAMESPACE,
+    CLASS_CLEAR_SELECT = '.qor-selected__remove',
+    CLASS_CHANGE_SELECT = '.qor-selected__change',
+    CLASS_SELECT_FIELD = '.qor-field__selected',
+    CLASS_SELECT_INPUT = '.qor-field__selectone-input',
+    CLASS_SELECT_TRIGGER = '.qor-field__selectone-trigger',
+    CLASS_PARENT = '.qor-field__selectone',
+    CLASS_SELECTED = 'is_selected',
+    CLASS_ONE = 'qor-bottomsheets__select-one';
 
   function QorSelectOne(element, options) {
     this.$element = $(element);
@@ -41,33 +39,28 @@
   QorSelectOne.prototype = {
     constructor: QorSelectOne,
 
-    init: function () {
+    init: function() {
       this.bind();
     },
 
-    bind: function () {
-      $document.on(EVENT_CLICK, '[data-selectone-url]', this.openBottomSheets.bind(this)).
-                on(EVENT_RELOAD, '.' + CLASS_ONE, this.reloadData.bind(this));
+    bind: function() {
+      $document.on(EVENT_CLICK, '[data-selectone-url]', this.openBottomSheets.bind(this)).on(EVENT_RELOAD, '.' + CLASS_ONE, this.reloadData.bind(this));
 
-      this.$element.
-        on(EVENT_CLICK, CLASS_CLEAR_SELECT, this.clearSelect.bind(this)).
-        on(EVENT_CLICK, CLASS_CHANGE_SELECT, this.changeSelect);
+      this.$element.on(EVENT_CLICK, CLASS_CLEAR_SELECT, this.clearSelect.bind(this)).on(EVENT_CLICK, CLASS_CHANGE_SELECT, this.changeSelect);
     },
 
-    unbind: function () {
-      $document.off(EVENT_CLICK, '[data-selectone-url]', this.openBottomSheets.bind(this)).
-                off(EVENT_RELOAD, '.' + CLASS_ONE, this.reloadData.bind(this));
+    unbind: function() {
+      $document.off(EVENT_CLICK, '[data-selectone-url]', this.openBottomSheets.bind(this)).off(EVENT_RELOAD, '.' + CLASS_ONE, this.reloadData.bind(this));
 
-      this.$element.
-        off(EVENT_CLICK, CLASS_CLEAR_SELECT, this.clearSelect.bind(this)).
-        off(EVENT_CLICK, CLASS_CHANGE_SELECT, this.changeSelect);
+      this.$element.off(EVENT_CLICK, CLASS_CLEAR_SELECT, this.clearSelect.bind(this)).off(EVENT_CLICK, CLASS_CHANGE_SELECT, this.changeSelect);
     },
 
-    clearSelect: function (e) {
+    clearSelect: function(e) {
       var $target = $(e.target),
-          $parent = $target.closest(CLASS_PARENT);
+        $parent = $target.closest(CLASS_PARENT);
 
       $parent.find(CLASS_SELECT_FIELD).remove();
+      $parent.find(CLASS_SELECT_INPUT).html('');
       $parent.find(CLASS_SELECT_INPUT)[0].value = '';
       $parent.find(CLASS_SELECT_TRIGGER).show();
 
@@ -75,20 +68,18 @@
       return false;
     },
 
-    changeSelect: function () {
+    changeSelect: function() {
       var $target = $(this),
-          $parent = $target.closest(CLASS_PARENT);
+        $parent = $target.closest(CLASS_PARENT);
 
       $parent.find(CLASS_SELECT_TRIGGER).trigger('click');
-
     },
 
-    openBottomSheets: function (e) {
+    openBottomSheets: function(e) {
       var $this = $(e.target),
-          data = $this.data();
+        data = $this.data();
 
       this.BottomSheets = $body.data('qor.bottomsheets');
-      this.bottomsheetsData = data;
       this.$parent = $this.closest(CLASS_PARENT);
 
       data.url = data.selectoneUrl;
@@ -98,9 +89,9 @@
       this.BottomSheets.open(data, this.handleSelectOne.bind(this));
     },
 
-    initItem: function () {
+    initItem: function() {
       var $selectFeild = this.$parent.find(CLASS_SELECT_FIELD),
-          selectedID;
+        selectedID;
 
       if (!$selectFeild.length) {
         return;
@@ -113,47 +104,46 @@
       }
     },
 
-    reloadData: function () {
+    reloadData: function() {
       this.initItem();
     },
 
-    renderSelectOne: function (data) {
+    renderSelectOne: function(data) {
       return Mustache.render($('[name="select-one-selected-template"]').html(), data);
     },
 
-    handleSelectOne: function ($bottomsheets) {
+    handleSelectOne: function($bottomsheets) {
       var options = {
         onSelect: this.onSelectResults.bind(this), //render selected item after click item lists
-        onSubmit: this.onSubmitResults.bind(this)  //render new items after new item form submitted
+        onSubmit: this.onSubmitResults.bind(this) //render new items after new item form submitted
       };
 
-      $bottomsheets.qorSelectCore(options).addClass(CLASS_ONE).data(this.bottomsheetsData);
+      $bottomsheets.qorSelectCore(options).addClass(CLASS_ONE);
       this.$bottomsheets = $bottomsheets;
       this.initItem();
     },
 
-    onSelectResults: function (data) {
+    onSelectResults: function(data) {
       this.handleResults(data);
     },
 
-    onSubmitResults: function (data) {
+    onSubmitResults: function(data) {
       this.handleResults(data, true);
     },
 
-    handleResults: function (data, isNewData) {
+    handleResults: function(data) {
       var template,
-          bottomsheetsData = this.bottomsheetsData,
-          $parent = this.$parent,
-          $select = bottomsheetsData.selectId ? $(bottomsheetsData.selectId) : $parent.find('select'),
-          $selectFeild = $parent.find(CLASS_SELECT_FIELD);
+        $parent = this.$parent,
+        $select = $parent.find('select'),
+        $selectFeild = $parent.find(CLASS_SELECT_FIELD);
 
       data.displayName = data.Text || data.Name || data.Title || data.Code || data[Object.keys(data)[0]];
+      data.selectoneValue = data.primaryKey || data.ID;
 
       if (!$select.length) {
         return;
       }
 
-      $select[0].value = data.primaryKey;
       template = this.renderSelectOne(data);
 
       if ($selectFeild.length) {
@@ -163,27 +153,24 @@
       $parent.prepend(template);
       $parent.find(CLASS_SELECT_TRIGGER).hide();
 
-      if (isNewData) {
-        $select.append(Mustache.render(QorSelectOne.SELECT_ONE_OPTION_TEMPLATE, data));
-        $select[0].value = data.primaryKey;
-      }
+      $select.html(Mustache.render(QorSelectOne.SELECT_ONE_OPTION_TEMPLATE, data));
+      $select[0].value = data.primaryKey || data.ID;
 
       $parent.trigger('qor.selectone.selected', [data]);
 
       this.$bottomsheets.qorSelectCore('destroy').remove();
     },
 
-    destroy: function () {
+    destroy: function() {
       this.unbind();
       this.$element.removeData(NAMESPACE);
     }
-
   };
 
-  QorSelectOne.SELECT_ONE_OPTION_TEMPLATE = '<option value="[[ primaryKey ]]" >[[ displayName ]]</option>';
+  QorSelectOne.SELECT_ONE_OPTION_TEMPLATE = '<option value="[[ selectoneValue ]]" selected>[[ displayName ]]</option>';
 
-  QorSelectOne.plugin = function (options) {
-    return this.each(function () {
+  QorSelectOne.plugin = function(options) {
+    return this.each(function() {
       var $this = $(this);
       var data = $this.data(NAMESPACE);
       var fn;
@@ -196,24 +183,23 @@
         $this.data(NAMESPACE, (data = new QorSelectOne(this, options)));
       }
 
-      if (typeof options === 'string' && $.isFunction(fn = data[options])) {
+      if (typeof options === 'string' && $.isFunction((fn = data[options]))) {
         fn.apply(data);
       }
     });
   };
 
-  $(function () {
+  $(function() {
     var selector = '[data-toggle="qor.selectone"]';
-    $(document).
-      on(EVENT_DISABLE, function (e) {
+    $(document)
+      .on(EVENT_DISABLE, function(e) {
         QorSelectOne.plugin.call($(selector, e.target), 'destroy');
-      }).
-      on(EVENT_ENABLE, function (e) {
+      })
+      .on(EVENT_ENABLE, function(e) {
         QorSelectOne.plugin.call($(selector, e.target));
-      }).
-      triggerHandler(EVENT_ENABLE);
+      })
+      .triggerHandler(EVENT_ENABLE);
   });
 
   return QorSelectOne;
-
 });
